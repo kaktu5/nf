de1e8ab520c99da5035c4ca36d6830c9853bad73
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-04-26 18:14:07 +0200

    luarc

--- neovim/lua/lang/lua.lua
+++ neovim/lua/lang/lua.lua
@@ -2,20 +2,7 @@ return {
   filetypes = { "lua" },
 
   lsp = {
-    lua_ls = {
-      cmd = { "lua-language-server" },
-      settings = {
-        Lua = {
-          runtime = { version = "LuaJIT" },
-          workspace = {
-            checkThirdParty = false,
-            library = { vim.env.VIMRUNTIME },
-          },
-          hint = { enable = true },
-          codeLens = { enable = true },
-        },
-      },
-    },
+    lua_ls = { cmd = { "lua-language-server" } },
   },
 
   lint = { "selene" },
