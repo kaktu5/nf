33350a64859997990b7a0517623035077e021b24
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-04-26 18:14:07 +0200

    wrap neovim

--- pkgs/neovim.nix
+++ pkgs/neovim.nix
@@ -5,13 +5,35 @@
   stdenv,
 }: let
   inherit (inputs.neovim-nightly-overlay.packages.${system}) neovim;
+  inherit (lib.attrsets) mapAttrs;
+  inherit (lib.fileset) toSource;
+  inherit (lib.lists) singleton;
   inherit (lib.mnw) wrap;
   inherit (stdenv.hostPlatform) system;
+
+  npinsToPlugins = input: let
+    fetchPin = _: pin: pin {inherit pkgs;};
+    npins' = import ../npins {inherit input;};
+  in
+    mapAttrs fetchPin npins';
 in
   wrap pkgs {
     inherit neovim;
 
+    aliases = ["vi" "vim"];
+
     initLua = ''
-      print("Hello, World!");
+      require("kkts");
+      require("lz.n").load("lazy");
     '';
+
+    plugins = {
+      start = singleton (toSource {
+        root = ../neovim;
+        fileset = ../neovim;
+      });
+
+      startAttrs = npinsToPlugins ../npins/start.json;
+      optAttrs = npinsToPlugins ../npins/opt.json;
+    };
   }
