e30b01a4a2943323038687be3f67edb07ac3593d
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-04-26 18:14:07 +0200

    switch to mdformat

--- internal/formatter.nix
+++ internal/formatter.nix
@@ -8,11 +8,11 @@ in
   writeShellApplication {
     name = "nf-nix3-fmt-wrapper";
     runtimeInputs = attrValues {
-      inherit (pkgs) alejandra deno fd stylua;
+      inherit (pkgs) alejandra fd mdformat stylua;
     };
     text = ''
       fd "$@" -t f -e lua -X stylua '{}'
-      fd "$@" -t f -e md -X deno fmt '{}'
+      fd "$@" -t f -e md -X mdformat '{}'
       fd "$@" -t f -e nix -E npins/ -X alejandra --quiet '{}'
     '';
   }
