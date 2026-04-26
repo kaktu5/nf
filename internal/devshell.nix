b0c9609cb8cc089236110d83eb3fbdc0f7c3ed79
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-04-26 18:14:07 +0200

    add missing packages to devshell

--- internal/devshell.nix
+++ internal/devshell.nix
@@ -8,7 +8,17 @@ in
   mkShellNoCC {
     name = "nf-devshell";
     packages = attrValues {
+      # lua
+      inherit (pkgs) lua-language-server stylua;
+      inherit (pkgs.luajitPackages) luacheck;
+
+      # markdown
+      inherit (pkgs) markdownlint-cli2 marksman mdformat;
+
       # nix
       inherit (pkgs) alejandra dix nixd npins;
+
+      # toml
+      inherit (pkgs) taplo tombi;
     };
   }
