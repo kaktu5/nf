be9a73d28d6193329a3ce2eb33c9c845ef20403e
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-08-16 14:15:08 +0200

    update

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+  inputs.nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
 
   outputs = {nixpkgs, ...} @ inputs: let
     sources = import ./npins;
